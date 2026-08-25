3314bbc6c1624eeb949214a071559d128ba4d1f0
Webert Maximiano <webertcoach@gmail.com>
2026-08-24 21:41:28 -0300

    arquivos exemplos

---
+++ alunos/Webert/aula.js
@@ -0,0 +1,18 @@
+const nomeDoUtilizador = "Webert"; // Variável que não pode ser alterada
+let pontosNoJogo = 10; // Variável que pode ser alterada
+var nivelDeDificuldade = "Médio"; // Variável que pode ser alterada, mas não é recomendada para uso moderno
+
+let x = 5; // Variável que pode ser alterada
+let y = 10; // Variável que pode ser alterada
+
+// Função para somar dois números
+//jsskf
+function somar(x, y) {
+    return x + y;
+}
+
+/* Função para subtrair dois números fdjfdkf
+*/
+function subtrair(x, y) {
+    return x - y;
+}
